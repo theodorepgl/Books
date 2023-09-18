305bd0873bfbdda85d84329225021d8b581fc3dd
theodorepgl <limlimtheo96@gmail.com>
2023-09-18 16:59:34 +0800

    First Commit

--- app.rb
+++ app.rb
@@ -1,2 +1,28 @@
 require 'sinatra'
 require 'sinatra/activerecord'
+
+require_relative 'models/book'
+
+get '/' do
+	erb :index
+end
+
+post '/add-book' do 
+	@book = Book.create(title: params[:title], author: params[:author])
+
+	if @book.save
+		erb :book 
+	else
+		erb :index
+	end
+end
+
+get '/books/:id'do
+	@book = Book.find(params[:id])
+	erb :book
+end
+
+get '/books' do 
+	@books = Book.all 
+	erb :books
+end
---
+++ models/book.rb
@@ -0,0 +1,4 @@
+class Book < ActiveRecord::Base
+	validates_presence_of :title
+	validates_presence_of :author
+end
