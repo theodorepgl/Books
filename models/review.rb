42a8b83fefcdb77ba3de10ef22bfd881c8a0558c
theodorepgl <limlimtheo96@gmail.com>
2023-09-20 13:13:45 +0800

    20.09.2023

--- models/review.rb
+++ models/review.rb
@@ -1,9 +1,3 @@
 class Review < ActiveRecord::Base
 	belongs_to :book, required: true
-
-	validates :score, numbericality: {
-		only_integer: true,
-		greateer_than_or_equal_to: 1,
-		less_than_or_equal_to: 5
-	}
 end
